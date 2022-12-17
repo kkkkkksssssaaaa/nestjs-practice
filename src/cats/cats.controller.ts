21b7c0778b21e4edd1403a82836d8ee01d62a690
kkkkkksssssaaaa <kofc312@naver.com>
2022-12-18 00:50:01 +0900

    feat(cat.controller): add save method

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+
+  @Post()
+  @HttpCode(204)
+  create(): string {
+    return 'This action adds a new cat';
+  }
 
   @Get()
   findAll(): string {
