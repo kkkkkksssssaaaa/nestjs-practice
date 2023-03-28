74a2682ee19a07214d4a9746dc116ebea18ac433
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 22:38:41 +0900

    docs(app-module): add annotations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
+  // 애플리케이션에서 사용될 모듈 
   imports: [BoardsModule],
   // 모듈에서 사용할 컨트롤러 클래스 지정, Root 모듈에 해당 애플리케이션에서 사용될 컨트롤러 클래스를 모두 지정
   controllers: [],
