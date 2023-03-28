b31887de4eb7515997c394f3af0b9601f965bce9
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 23:20:37 +0900

    refactor(boards-service): rename wrong property

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -3,9 +3,9 @@ import { Board } from '../../dist/boards/board.model';
 
 @Injectable()
 export class BoardsService {
-  private boareds: Board[] = [];
+  private boards: Board[] = [];
 
   getAllBoards(): Board[] {
-    return this.boareds;
+    return this.boards;
   }
 }
