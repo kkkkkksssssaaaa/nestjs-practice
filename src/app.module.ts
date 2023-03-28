a4c18907289517af49efbf1b63250b8cc43d613d
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 22:34:27 +0900

    feat: add boards module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
   // 모듈에서 사용할 컨트롤러 클래스 지정, Root 모듈에 해당 애플리케이션에서 사용될 컨트롤러 클래스를 모두 지정
   controllers: [],
   // 모듈에서 사용할 서비스 클래스 지정
