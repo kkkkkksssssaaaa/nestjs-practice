fbf8f58b5ee68e971f2c91faf80dd3565e6620af
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 22:24:25 +0900

    docs(app-module): add annotations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
+  // 모듈에서 사용할 컨트롤러 클래스 지정, Root 모듈에 해당 애플리케이션에서 사용될 컨트롤러 클래스를 모두 지정
   controllers: [AppController],
+  // 모듈에서 사용할 서비스 클래스 지정
   providers: [AppService],
 })
 export class AppModule {}
