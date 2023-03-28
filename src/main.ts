835a40d1853376b2e5dc127f147921aabe1dd6c7
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 22:15:15 +0900

    docs(main): add annotations

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Root Module 지정
   const app = await NestFactory.create(AppModule);
+  // Nest.js 애플리케이션 포트 지정
   await app.listen(3000);
 }
 bootstrap();
