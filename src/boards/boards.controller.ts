df2483756cbf6ec5bf6a60a0ad8cf91c6b40962d
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 22:58:34 +0900

    feat(board-controller): add constructor with DI

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 // 컨트롤러 클래스임을 명시하는 데코레이터
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private service: BoardsService) {}
+}
