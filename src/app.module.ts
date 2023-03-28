d1c8eb1319800b4b757ba3f09f72d5b98a3a5cde
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 22:33:18 +0900

    chore: remove files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
   // 모듈에서 사용할 컨트롤러 클래스 지정, Root 모듈에 해당 애플리케이션에서 사용될 컨트롤러 클래스를 모두 지정
-  controllers: [AppController],
+  controllers: [],
   // 모듈에서 사용할 서비스 클래스 지정
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
