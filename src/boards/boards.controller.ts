9172f7f9df499d6fb184e8b79656c0ec0475d474
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-29 23:39:29 +0900

    feat(boards-controller): add create board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from '../../dist/boards/board.model';
 
@@ -10,5 +10,14 @@ export class BoardsController {
   @Get()
   getAllBoards(): Board[] {
     return this.service.getAllBoards();
+  }
+
+  // json object 를 받으려면... @Body() 를 사용하여 전달받음
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.service.createBoard(title, description);
   }
 }
