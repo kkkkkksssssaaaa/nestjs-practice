897eb96af293ff1bd60075fde5f219f444893398
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-31 23:20:15 +0900

    feat(boards-controller): add get board by id

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -11,6 +11,11 @@ export class BoardsController {
   @Get()
   getAllBoards(): Board[] {
     return this.service.getAllBoards();
+  }
+
+  @Get(':id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.service.getBoardById(id);
   }
 
   // json object 를 받으려면... @Body() 를 사용하여 전달받음
