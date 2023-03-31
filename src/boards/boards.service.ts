81cdcaed70e4f0e2679c2bcd3bc1419c34717d8b
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-31 23:18:42 +0900

    feat(boards-service): add get board by id

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -11,6 +11,10 @@ export class BoardsService {
     return this.boards;
   }
 
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
   createBoard(dto: CreateBoardDto) {
     const board: Board = {
       id: randomUUID(),
