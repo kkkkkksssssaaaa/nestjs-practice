eccbb0e13e0951d85a85b3ed6b78fbe5192e66df
kkkkkksssssaaaa <kofc312@naver.com>
2023-04-12 23:25:15 +0900

    feat(board-repository): add create board method

--- src/boards/repository/board.repository.ts
+++ src/boards/repository/board.repository.ts
@@ -1,5 +1,18 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Board } from '../entity/board.entity';
+import { CreateBoardDto } from '../dto/create-board.dto';
+import { BoardStatus } from '../model/board-status.enum';
 
-@EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+export class BoardRepository extends Repository<Board> {
+  async createBoard(dto: CreateBoardDto): Promise<Board> {
+    const board: Board = this.create({
+      title: dto.title,
+      description: dto.description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    await this.save(board);
+
+    return board;
+  }
+}
