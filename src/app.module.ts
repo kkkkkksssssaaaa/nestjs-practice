9773d39b9b6f7f21da4b9833724ccf2c9f784405
kkkkkksssssaaaa <kofc312@naver.com>
2023-04-13 23:07:52 +0900

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   // 애플리케이션에서 사용될 모듈
-  imports: [BoardsModule, TypeOrmModule.forRoot(typeORMConfig)],
+  imports: [BoardsModule, TypeOrmModule.forRoot(typeORMConfig), AuthModule],
   // 모듈에서 사용할 컨트롤러 클래스 지정, Root 모듈에 해당 애플리케이션에서 사용될 컨트롤러 클래스를 모두 지정
   controllers: [],
   // 모듈에서 사용할 서비스 클래스 지정
