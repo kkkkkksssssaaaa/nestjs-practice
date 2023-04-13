8f0a4ab81f0fe7b1d27674a3b5fedf703674965c
kkkkkksssssaaaa <kofc312@naver.com>
2023-04-13 23:13:39 +0900

    feat: inject user repository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './repository/user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: UserRepository,
+  ) {}
+}
