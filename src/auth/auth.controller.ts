41af9831400d005d4a6bc8db70455e3db4598eb3
kkkkkksssssaaaa <kofc312@naver.com>
2023-04-28 23:53:16 +0900

    feat: add sign in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,4 +10,9 @@ export class AuthController {
   async signUp(@Body(ValidationPipe) dto: AuthCredentialsDto): Promise<void> {
     await this.service.signUp(dto);
   }
+
+  @Post('/signin')
+  async signIn(@Body(ValidationPipe) dto: AuthCredentialsDto): Promise<void> {
+    await this.service.signIn(dto);
+  }
 }
