d29d4d96ca4f8f7f3e5353046a3fa3ecd4d46703
kkkkkksssssaaaa <kofc312@naver.com>
2023-05-12 22:46:31 +0900

    feat: add test api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +24,11 @@ export class AuthController {
     accessToken: string;
   }> {
     return await this.service.signIn(dto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req) {
+    console.log('req', req);
   }
 }
