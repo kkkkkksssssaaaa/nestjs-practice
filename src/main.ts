9592a1df100d002f2a5bbbce75e03071d9b8f490
kkkkkksssssaaaa <kofc312@naver.com>
2023-05-17 23:11:01 +0900

    feat: use logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const portNumber = 3000;
   // Root Module 지정
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'error', 'log', 'verbose', 'warn'],
+  });
   // Nest.js 애플리케이션 포트 지정
-  await app.listen(3000);
+  await app.listen(portNumber);
 }
 bootstrap();
